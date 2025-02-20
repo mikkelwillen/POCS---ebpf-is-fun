6a38a4ecdd6575bb42d09736f1b0369b17abfed8
Mikkel Willén <mikkel@willen.dk>
2025-02-20 15:28:05 +0100

    works with new structure

---
+++ socket-filters/filters/simple-socket-filter/src/main.rs
@@ -0,0 +1,32 @@
+#![no_std]
+#![no_main]
+
+// Import the `socket_filter` macro used to specify the entry point of the eBPF program
+// Import the `SkBuffContext` struct with metadata and payload
+use aya_ebpf::{
+    macros::socket_filter,
+    programs::SkBuffContext
+};
+
+// Filter that lets all packets through
+#[socket_filter]
+pub fn socket_filter(ctx: SkBuffContext) -> i64 {
+    -1
+}
+//     match try_socket_filter(ctx) {
+//         0 => -1,
+//         _ => 0,
+//     }
+// }
+
+// Helper function
+// fn try_socket_filter(ctx: SkBuffContext) -> i64 {
+//     0
+// }
+
+// Simple panic handler
+#[cfg(not(test))]
+#[panic_handler]
+fn panic(_info: &core::panic::PanicInfo) -> ! {
+    loop {}
+}
